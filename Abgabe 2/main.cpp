478a4fc2fbeab2e115c2964485879e6639d0c076
stykespike <32951207+stykespike@users.noreply.github.com>
2018-01-18 15:30:06 +0100

    main aktualisiert

--- Abgabe 2/main.cpp
+++ Abgabe 2/main.cpp
@@ -22,27 +22,25 @@ void trieend(Trie<int> &trie);
 
 int main(int argc, char* argv[]){
     Trie<int> trie1;
-    int i;
-    int g;
     int command;
     while(command != 0){
 	std::cout << "-----------------<Modify Your Trie>----------------" << '\n'
-			<< "*------<Commands>--------*" << '\n'
-			<< "|0: exit the programm    |" << '\n'
-			<< "|1: empty()              |"	<< '\n'
-			<< "|2: insert()             |"	<< '\n'
-			<< "|3: erase()              |"	<< '\n'
-			<< "|4: clear()              |"	<< '\n'
-                	<< "|5: print()              |"	<< '\n'
-                	<< "|6: lower_bound()        |"	<< '\n'
-                	<< "|7: upper_bound()        |"	<< '\n'
-			<< "|8: find()               |"	<< '\n'
-			<< "|9: begin()              |" << '\n'
-			<< "|10: end()               |" << '\n'
-			<< "*------------------------*"	<< '\n'
-			<< "Enter number here: ";
+			  << "*------<Commands>--------*"	<< std::endl
+			  << "|0: exit the programm    |"	<< std::endl
+			  << "|1: empty()              |"	<< std::endl
+			  << "|2: insert()             |"	<< std::endl
+			  << "|3: erase()              |"	<< std::endl
+			  << "|4: clear()              |"	<< std::endl
+              << "|5: print()              |"	<< std::endl
+              << "|6: lower_bound()        |"	<< std::endl
+              << "|7: upper_bound()        |"	<< std::endl
+			  << "|8: find()               |"	<< std::endl
+			  << "|9: begin()              |"	<< std::endl
+			  << "|10: end()               |"	<< std::endl
+			  << "*------------------------*"	<< std::endl
+			  << "Enter number here: ";
 	std::cin >> command;
-        std::cout << '\n';
+        std::cout << std::endl;
         try{
 		if(command == 1){
 			trieempty(trie1);
@@ -76,85 +74,81 @@ int main(int argc, char* argv[]){
 		}
 	}
 	catch (const char* exception){
-		std::cerr << "Error: " << exception << '\n';
+		std::cerr << "Error: " << exception << std::endl;
 	}
-	std::cout << '\n';
+	std::cout << std::endl;
     }
-    //trie1.insert(std::make_pair("hallo", 99));
-    //trie1.insert(std::make_pair("hales", 13));
-    //trie1.insert(std::make_pair("has", 0));
-    //trie1.print();
-    //trie1.erase("has");
-    //std::cin >> i;
-    //trie1.clear();
-    //trie1.print();
-    //std::cin >> g;
     return 0;
 }
 
 void trieempty(Trie<int> &trie){
-    std::cout << (trie.empty()? "Trie is empty" : "Trie is not empty") << '\n';
+    std::cout << (trie.empty()? "Trie is empty" : "Trie is not empty") << std::endl;
 }
 
-void trieinsert(Trie<int> &trie){
+void trieinsert(Trie<int> &trie) {
     std::string input;
     int index;
-    std::cout << "Enter input" << '\n';
+    std::cout << "Enter key: ";
 	std::cin >> input;
-	std::cout << "Enter position to insert at: ";
+	std::cout << std::endl;
+	std::cout << "Enter value: ";
 	std::cin >> index;
-        std::cout << '\n';
-    trie.insert(std::make_pair(input, index));
-    std::cout << "Element has been inserted" << '\n';
+    std::cout << std::endl;
+    auto itr = trie.insert(std::make_pair(input, index));
+    std::cout << itr.getKey() << "->" << *itr << std::endl;
+    std::cout << "Element has been inserted" << std::endl;
 }
 
 void trieerase(Trie<int> &trie){
     std::string eraser;
-    std::cout << "Enter word to delete" << '\n';
+    std::cout << "Enter key to delete" << std::endl;
     std::cin >> eraser;
     trie.erase(eraser);
-    std::cout << "Element has been removed" << '\n';
+    std::cout << "Element has been removed" << std::endl;
 }
 
 void trieclear(Trie<int> &trie){
     trie.clear();
-    std::cout << "Trie cleared!" << '\n';
+    std::cout << "Trie cleared!" << std::endl;
 }
 
 void trieprint(Trie<int> &trie){
-    trie.print();
+	trie.print();
+    for (auto itr = trie.begin(); itr != trie.end(); itr++) {
+    	std::cout << itr.getKey() << "->" << *itr << std::endl;
+    }
 }
 
 void trielower(Trie<int> &trie){
     std::string lower;
-    std::cout << "Enter element to get first position" << '\n';
+    std::cout << "Enter key to get first position" << std::endl;
     std::cin >> lower;
-    auto itrfind = trie.find(lower);
-    std::cout << *itrfind << '\n';
+    auto itrfind = trie.lower_bound(lower);
+    std::cout << itrfind.getKey() << "->" <<*itrfind << std::endl;
 }
 
 void trieupper(Trie<int> &trie){
     std::string upper;
-    std::cout << "Enter element to get last position" << '\n';
+    std::cout << "Enter key to get last position" << std::endl;
     std::cin >> upper;
-    auto itrfind = trie.find(upper);
-    std::cout << *itrfind << '\n';
+    auto itrfind = trie.upper_bound(upper);
+    std::cout<< itrfind.getKey() << "->" << *itrfind << std::endl;
 }
 
 void triefind(Trie<int> &trie){
     std::string find;
-    std::cout << "Enter element to receive position" << '\n';
+    std::cout << "Enter element to receive position" << std::endl;
     std::cin >> find;
     auto itrfind = trie.find(find);
-    std::cout << *itrfind << '\n';
+    std::cout << *itrfind << std::endl;
 }
 
 void triebegin(Trie<int> &trie){
     auto itr1 = trie.begin();
-    std::cout << "Beginning iterator: " << *itr1 << '\n';
+    std::cout << "Beginning iterator: " << itr1.getKey() << "->" <<*itr1 << std::endl;
 }
 
 void trieend(Trie<int> &trie){
-    auto itr2 = trie.begin();
-    std::cout << "End iterator: " << *itr2 << '\n';
+    auto itr2 = trie.end();
+    std::cout << "End iterator: " << itr2.getKey() << "->" << *itr2 << '\n';
 }
